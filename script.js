31c0120730f3f542cd63db302a37bd35903b7fa0
yoshiko <ttkmysk021@gmail.com>
2023-05-15 21:16:00 +0900

    first creat

---
+++ script.js
@@ -0,0 +1,10 @@
+var text = "どちらでもない";
+if(text=="はい"){
+  console.log("true");
+  console.log(text);
+}else if(text == "いいえ"){
+  console.log("false");
+  console.log(text);
+}else{
+  console.log("どちらでもない");
+}
